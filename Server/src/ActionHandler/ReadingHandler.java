8e4c845af8011e47efa8e9aa08306b5895ffa3d9
FrancoisMontigny <francois.montigny@gmail.com>
2019-09-23 18:03:51 -0400

    feat: fixed writing of attributes

--- Server/src/ActionHandler/ReadingHandler.java
+++ Server/src/ActionHandler/ReadingHandler.java
@@ -4,31 +4,31 @@ import PersistingHandler.PersistingHandlerInterface;
 
 import java.lang.reflect.Method;
 
-public class ReadingHandler extends ActionHandler implements ReadingHandlerInterface{
+public class ReadingHandler extends ActionHandler implements ReadingHandlerInterface {
 
-    public ReadingHandler(PersistingHandlerInterface persistingHandler) {
-        super(persistingHandler);
-    }
+	public ReadingHandler(PersistingHandlerInterface persistingHandler) {
+		super(persistingHandler);
+	}
 
-    @Override
-    public String treat(String instanceName, String attribute) {
+	@Override
+	public String treat(String instanceName, String attribute) {
 
-        log("Treating a reading");
-        attribute = this.upFirstLetter(attribute);
-        Object object = persistingHandler.get(instanceName);
-        Class objectClass = object.getClass();
-        try {
-            Method objectMethod = objectClass.getMethod("get" + attribute);
-            return (String)objectMethod.invoke(object);
-        } catch (Exception e) {
-            log(e.getMessage());
-            return "Impossible to read the attribute " + attribute + " of " + instanceName + " : " + e.getMessage();
-        }
-    }
+		log("Treating a reading");
+		attribute = this.upFirstLetter(attribute);
+		Object object = persistingHandler.get(instanceName);
+		Class objectClass = object.getClass();
+		try {
+			Method objectMethod = objectClass.getMethod("get" + attribute);
+			return (String) objectMethod.invoke(object);
+		} catch (Exception e) {
+			log(e.getMessage());
+			return "Impossible to read the attribute " + attribute + " of " + instanceName + " : " + e.getMessage();
+		}
+	}
 
-    private String upFirstLetter(String string) {
-        char[] attributeArray = string.toCharArray();
-        attributeArray[0] = Character.toUpperCase(attributeArray[0]);
-        return new String(attributeArray);
-    }
+	private String upFirstLetter(String string) {
+		char[] attributeArray = string.toCharArray();
+		attributeArray[0] = Character.toUpperCase(attributeArray[0]);
+		return new String(attributeArray);
+	}
 }
--- Server/src/ActionHandler/WritingHandler.java
+++ Server/src/ActionHandler/WritingHandler.java
@@ -17,9 +17,14 @@ public class WritingHandler extends ActionHandler implements WritingHandlerInter
 		attribute = this.upFirstLetter(attribute);
 		Object object = this.persistingHandler.get(instanceName);
 		Class objectClass = object.getClass();
+		
+		// We're setting an attribute, so the length of the array can be one.
+		Class[] parameters = new Class[1];
+		parameters[0] = value.getClass();
+		
 		try {
-			Method objectMethod = objectClass.getMethod("set" + attribute);
-			return (String) objectMethod.invoke(object, value);
+			Method objectMethod = objectClass.getMethod("set" + attribute, parameters);
+			return "Success when writing " + value + " in attribute " + attribute + " of instance " + instanceName;
 		} catch (Exception e) {
 			log(e.getMessage());
 			return "Impossible to write the attribute " + attribute + " of " + instanceName + " : " + e.getMessage();
